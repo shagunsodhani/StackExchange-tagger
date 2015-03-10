f1429109b31fba2292824abab2b2a9b614e96fc1
Shagun <sshagunsodhani@gmail.com>
2015-03-10 16:45:30 +0530

    Added stemmer and lemmatizer

--- app/parser.py
+++ app/parser.py
@@ -14,11 +14,17 @@ try:
 except ImportError as exc:
     print("Error: failed to import settings module ({})".format(exc))
 
-dataset = str(sys.argv[1])
+try:
+    import nltk
+except ImportError as exc:
+    print("Error: failed to import settings module ({})".format(exc))
+
+test_data = "data/processed.csv"
+stopword_data = "data/stopword.txt"
 
 def remove_blanks():
 	count = 0
-	with open(dataset) as infile:
+	with open(test_data) as infile:
 		for line in infile:
 			if(line[-3:]=="\"\r\n"):
 				#End of one post
@@ -26,6 +32,29 @@ def remove_blanks():
 			else:
 				print line.strip(),
 			count+=1
-	print count
+	# print count
 
-remove_blanks()
+def remove_stopwords():
+
+	porter_stemmer = nltk.stem.porter.PorterStemmer()
+	wordnet_lemmatizer = nltk.stem.WordNetLemmatizer()
+
+	stopwords = {}
+	with open(stopword_data) as infile:
+		for line in infile:
+			a = line.strip()
+			if a not in stopwords:
+				stopwords[a] = 1
+
+	with open(test_data) as infile:
+		for line in infile:
+			striped_line = line.strip()
+			if striped_line:
+				body = striped_line.split(",", 2)[2]
+				list_token = nltk.word_tokenize(body)
+				for token in list_token:
+					processed_token = wordnet_lemmatizer.lemmatize(porter_stemmer.stem(token))
+					print processed
+				
+# remove_blanks()
+remove_stopwords()
