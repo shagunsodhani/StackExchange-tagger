0c3003e16010c4150cff6cb92842320ea7e619ee
Shagun Sodhani <sshagunsodhani@gmail.com>
2015-03-10 19:26:52 +0530

    Added beautifulsoup to remove HTML Tags

--- app/parser.py
+++ app/parser.py
@@ -19,16 +19,21 @@ try:
 except ImportError as exc:
     print("Error: failed to import settings module ({})".format(exc))
 
+try:
+	from bs4 import BeautifulSoup
+except ImportError as exc:
+	print("Error: failed to import settings module ({})".format(exc))
+
 test_data = "data/processed.csv"
 stopword_data = "data/stopword.txt"
 
-def remove_blanks():
+def preprocess_dataset():
 	count = 0
 	with open(test_data) as infile:
 		for line in infile:
 			if(line[-3:]=="\"\r\n"):
 				#End of one post
-				print line
+				print line.strip()
 			else:
 				print line.strip(),
 			count+=1
@@ -50,12 +55,25 @@ def remove_stopwords():
 	with open(test_data) as infile:
 		for line in infile:
 			striped_line = line.strip()
-			if striped_line:
-				body = striped_line.split(",", 2)[2]
-				list_token = nltk.word_tokenize(body)
-				for token in list_token:
-					processed_token = wordnet_lemmatizer.lemmatize(porter_stemmer.stem(token))
-					print processed
+			if striped_line :
+				a = striped_line.split(',',2)
+				post_id = str(a[0])
+				title = str(a[1])
+				a = a[2].rsplit(',',1)
+				tag_list_string = a[1]
+				body = a[0]
+				#print body  
+				soup = BeautifulSoup(body)
+				body = soup.get_text()
+				print body
+			#if striped_line:
+				#print striped_line.split(',',4)
+			#if striped_line:
+			#	body = striped_line.split(",", 2)[2]
+			#	list_token = nltk.word_tokenize(body)
+			#	for token in list_token:
+			#		processed_token = wordnet_lemmatizer.lemmatize(porter_stemmer.stem(token))
+			#		print processed_token
 				
-# remove_blanks()
+#preprocess_dataset()
 remove_stopwords()
