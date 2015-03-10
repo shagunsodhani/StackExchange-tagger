a0b25cf8ce7a3a453c93f48dd6f8048679ad7bb3
Shagun <sshagunsodhani@gmail.com>
2015-03-10 11:22:43 +0530

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update
+apt-get install -y mongodb-org
+service mongod start
+
+pip install pymongo
